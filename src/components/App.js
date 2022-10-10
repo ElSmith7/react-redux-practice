1333a491ff6f1a15e803ce95be2bdaf164ae77e6
ElSmith7 <103047511+ElSmith7@users.noreply.github.com>
2022-10-10 19:33:20 +0100

    Added form and css

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,27 @@ function App() {
     <div className="App">
       <div className="wrapper">
         <h1>Bird List</h1>
+        <form>
+          <label>
+            <p>Add Bird</p>
+            <input type="text" />
+          </label>
+          <div>
+            <button type="submit">Add</button>
+          </div>
+        </form>
         <ul>
           {birds.map((bird) => (
             <li key={bird.name}>
               <h3>{bird.name}</h3>
-              <div>Views: {bird.views}</div>
+              <div>
+                Views: {bird.views}
+                <button>
+                  <span role="img" aria-label="add">
+                    ➕
+                  </span>
+                </button>
+              </div>
             </li>
           ))}
         </ul>
